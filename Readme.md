f624bf7c176ec95d265d394812be2ba7a129599a
RoscaStefan <roscastefan3@gmail.com>
2021-01-09 12:37:23 +0200

    Adaugare Readme si fisier .gitignore
    Signed-off-by: RoscaStefan <roscastefan3@gmail.com>

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## Wheather App
+Aceasta aplicatie afiseaza informatii meteorologice despre un oras, utilizand informatii funizate de catre UN API.
---
+++ src/main/java/ro/mta/se/lab/weatherApp/MainApp.java
@@ -0,0 +1,26 @@
+package ro.mta.se.lab.weatherApp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * @author Rosca Stefan
+ */
+
+public class MainApp extends Application {
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+
+
+
+}
